93461d09a0938429020a980fe0348a67944f8f96
Luis Morales <lmoralesma@gmail.com>
2021-02-22 06:23:14 -0500

    Adapting the script to run in bash  for one station

--- test/runme_tspre.sh
+++ test/runme_tspre.sh
@@ -3,29 +3,26 @@
 # Input data
 RUNM=0
 #INDIR=/home/luis/Documents/cpp/HYDATsfana/dayUnregLongFLOWSandCatch/
-INDIR=/home/luis/hub/canNatFlowRegi/data/
-OUTDIR=/home/luis/hub/canNatFlowRegi
-EXC=../bin/tspre
+INDIR=/home/luis/hub/canNatFlowRegi/data/raw/
+OUTDIR=/home/luis/hub/canNatFlowRegi/data/pretre/
+EXC=/home/luis/Documents/projects/hydrotsa/bin/tspre
 
 if [ $RUNM -eq 0 ]
 then
-	# Execute the code
 	#FILENAME=06AC006_DLY_FLOWS.csv
 	#FILENAME=01EO003_DLY_FLOWS.bin
-	FILENAME=05JM005_DLY_FLOWS.bin
-	$EXC $INDIR $FILENAME
+	INFILE=05JM005_DLY_FLOWS.bin
+	OUTFILE=$(cut -d. -f1 <<< ${INFILE})_pre.csv
 
-	# Copy output into a file
-	IFS='_'
-	read -a STATNAME <<< "$FILENAME"
-	mv ${INDIR}dre.csv $OUTDIR/data/dre_${STATNAME}.csv
+	# Execute the code
+	$EXC $INDIR $INFILE $OUTDIR $OUTFILE
 
 	# Plot, display and save into jpg
-	gnuplot -e "filename='$OUTDIR/data/dre_$STATNAME.csv'; outfile='$OUTDIR/plots/dre_$STATNAME.jpg'; statname='$STATNAME'" gpts
-	display -density 200  $OUTDIR/plots/dre_$STATNAME.jpg &
-	pid=$!
-	sleep 2
-	kill $pid
+	#gnuplot -e "filename='$OUTDIR/data/dre_$STATNAME.csv'; outfile='$OUTDIR/plots/dre_$STATNAME.jpg'; statname='$STATNAME'" gpts
+	#display -density 200  $OUTDIR/plots/dre_$STATNAME.jpg &
+	#pid=$!
+	#sleep 2
+	#kill $pid
 else
 	#for file in ${DIRNAME}*FLOWS.csv
 	for file in ${INDIR}*FLOWS.bin
