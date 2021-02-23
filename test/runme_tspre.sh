1fce0b322ba34c04632208aaafc7c0905f623f6e
Luis Morales <lmoralesma@gmail.com>
2021-02-23 06:17:20 -0500

    Set up the runme file to run tspre and the gnuplot scrip to plot original and preprocessed ts

--- test/runme_tspre.sh
+++ test/runme_tspre.sh
@@ -2,49 +2,56 @@
 
 # Input data
 RUNM=0
-#INDIR=/home/luis/Documents/cpp/HYDATsfana/dayUnregLongFLOWSandCatch/
+INDIRcsv=/home/luis/Documents/cpp/HYDATsfana/dayUnregLongFLOWSandCatch/
 INDIR=/home/luis/hub/canNatFlowRegi/data/raw/
 OUTDIR=/home/luis/hub/canNatFlowRegi/data/pretre/
+PLOTDIR=/home/luis/hub/canNatFlowRegi/plots/
 EXC=/home/luis/Documents/projects/hydrotsa/bin/tspre
 
 if [ $RUNM -eq 0 ]
 then
+	# Set variables
 	#FILENAME=06AC006_DLY_FLOWS.csv
 	#FILENAME=01EO003_DLY_FLOWS.bin
-	INFILE=05JM005_DLY_FLOWS.bin
+	INFILE=02HL003_DLY_FLOWS.bin
+	INFILEcsv=$(cut -d. -f1 <<< ${INFILE}).csv
+	PLOTNAME=$(cut -d. -f1 <<< ${INFILE})_pre.jpg
+	STNAME=$(cut -d_ -f1 <<< ${INFILE})
 	OUTFILE=$(cut -d. -f1 <<< ${INFILE})_pre.csv
 
 	# Execute the code
 	$EXC $INDIR $INFILE $OUTDIR $OUTFILE
 
 	# Plot, display and save into jpg
-	#gnuplot -e "filename='$OUTDIR/data/dre_$STATNAME.csv'; outfile='$OUTDIR/plots/dre_$STATNAME.jpg'; statname='$STATNAME'" gpts
-	#display -density 200  $OUTDIR/plots/dre_$STATNAME.jpg &
-	#pid=$!
-	#sleep 2
-	#kill $pid
+	if [ -f  "$OUTDIR$OUTFILE" ]; then
+		gnuplot -e "filename_csv='${INDIRcsv}${INFILEcsv}'; filename_pre='${OUTDIR}${OUTFILE}'; outfile='${PLOTDIR}${PLOTNAME}'; stname='${STNAME}'" tools/gpts_tspre
+		display -density 200 ${PLOTDIR}${PLOTNAME} &
+		pid=$!
+		sleep 2
+		kill $pid
+	fi
 else
 	#for file in ${DIRNAME}*FLOWS.csv
 	for file in ${INDIR}*FLOWS.bin
 	do
-		# Execute the code
-		FILENAME=$(echo "$file" | awk -F'/' '{print $NF}')
-		echo '------------->' $FILENAME
-		$EXC $INDIR $FILENAME
+		# Set variables
+		INFILE=$(echo "$file" | awk -F'/' '{print $NF}')
+		INFILEcsv=$(cut -d. -f1 <<< ${INFILE}).csv
+		PLOTNAME=$(cut -d. -f1 <<< ${INFILE})_pre.jpg
+		STNAME=$(cut -d_ -f1 <<< ${INFILE})
+		OUTFILE=$(cut -d. -f1 <<< ${INFILE})_pre.csv
 
-		# Copy output into a file
-		IFS='_'
-		read -a STATNAME <<< "$FILENAME"
-		mv ${INDIR}dre.csv $OUTDIR/data/dre_${STATNAME}.csv
-		#echo $STATNAME
-		IFS=''
+		# Execute the code
+		$EXC $INDIR $INFILE $OUTDIR $OUTFILE
 
 		# Plot, display and save into jpg
-		#gnuplot -e "filename='$OUTDIR/data/dre_$STATNAME.csv'; outfile='$OUTDIR/plots/dre_$STATNAME.jpg'; statname='$STATNAME'" gpts
-		#display -density 200  $OUTDIR/plots/dre_$STATNAME.jpg &
-		#pid=$!
-		#sleep 2
-		#kill $pid
+		if [ -f  "$OUTDIR$OUTFILE" ]; then
+			gnuplot -e "filename_csv='${INDIRcsv}${INFILEcsv}'; filename_pre='${OUTDIR}${OUTFILE}'; outfile='${PLOTDIR}${PLOTNAME}'; stname='${STNAME}'" tools/gpts_tspre
+			display -density 200 ${PLOTDIR}${PLOTNAME} &
+			pid=$!
+			sleep 2
+			kill $pid
+		fi
 
 	done #> out.txt
 fi
